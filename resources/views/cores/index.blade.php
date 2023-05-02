956209d05ec547ff0ef5978a70a5b796b414d8ae
Leonardo Oliveira <leoolliver77@gmail.com>
2023-05-02 13:18:06 -0300

    Estilizando Crud Cores

--- resources/views/cores/index.blade.php
+++ resources/views/cores/index.blade.php
@@ -1,25 +1,31 @@
-<h1>Cores</h1>
-<a href="{{route('cores.new')}}">Incluir</a>
-<table>
-    <thead>
-        <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>Criado em</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($cores as $c)
-        <tr>
-            <td>{{$c->codigo}}</td>
-            <td>{{$c->descricao}}</td>
-            <td>{{$c->created_at}}</td>
-            <td>
-                <a href="{{route('cores.edit', ['cor' => $c->codigo])}}">Editar</a>
-                <a href="{{route('cores.delete', ['codigo' => $c->codigo])}}">Excluir</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <a href="{{route('cores.new')}}" class="float-right btn btn-success">Incluir</a>
+    <h1 class="float-left">Cores</h1>
+    <table class="table table-stiped">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Criado em</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($cores as $c)
+            <tr>
+                <td>{{$c->codigo}}</td>
+                <td>{{$c->descricao}}</td>
+                <td>{{$c->created_at}}</td>
+                <td>
+                    <a href="{{route('cores.edit', ['cor' => $c->codigo])}}" class="btn btn-primary">Editar</a>
+                    <a href="{{route('cores.delete', ['codigo' => $c->codigo])}}" class="btn btn-danger">Excluir</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Contracts\Cache\Store;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -14,19 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
-Route::prefix('admin')->group(function(){
-  Route::get('produtos', 'Admin\\ProdutoController@index');
-  Route::get('produtos/new', 'Admin\\ProdutoController@store');
-});
-
-Route::prefix('admin')->group(function(){
+Route::group(['middleware' => ['auth']], function () {
+  Route::prefix('admin')->group(function () {
     Route::get('cores', 'Admin\\CorController@index')->name('cores.index');
     Route::get('cores/new', 'Admin\\CorController@new')->name('cores.new');
     Route::post('cores/store', 'Admin\\CorController@store')->name('cores.store');
     Route::get('cores/edit/{cor}', 'Admin\\CorController@edit')->name('cores.edit');
     Route::post('cores/update/{codigo}', 'Admin\\CorController@update')->name('cores.update');
     Route::get('cores/remove/{codigo}', 'Admin\\CorController@delete')->name('cores.delete');
+  });
+
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
